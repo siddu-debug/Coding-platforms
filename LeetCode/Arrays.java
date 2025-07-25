cabeb1f002d029b0a8ade93b972966f58b28deda
siddu-debug <siddurajgiri@gmail.com>
2025-07-25 14:34:07 +0530

    Update Arrays.java

--- LeetCode/Arrays.java
+++ LeetCode/Arrays.java
@@ -1,1 +1,33 @@
+import java.util.Scanner;
+class Student {
+    private int rollnum;
+    private String name;
+    private double marks1;
+    private double marks2;
+    private double marks3;
+    private double marks;
+    public Student(int rollnum, String name, double marks1, double marks2, double marks3) {
+        this.rollnum = rollnum;
+        this.name = name;
+        this.marks1 = marks1;
+        this.marks2 = marks2;
+        this.marks3 = marks3;
+        this.marks = marks1 + marks2 + marks3;
+    }
+    public void display() {
+        System.out.println(name);
+        System.out.println(rollnum);
+        System.out.println(marks);
+    }
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int rollnum = sc.nextInt();
+	double marks = 
+	int 
+	
+	
+
+        s.display();
+    }
+}
 
