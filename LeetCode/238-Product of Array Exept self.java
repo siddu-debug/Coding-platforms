e1d20fcecaa536506f025e331bcdd9ba6de0998d
siddu-debug <siddurajgiri@gmail.com>
2025-07-28 19:19:47 +0530

    Create 238-Product of Array Exept self.java

---
+++ LeetCode/238-Product of Array Exept self.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        int[] prod = new int[n];
+
+        // Update left array
+        left[0] = 1;
+        for (int i = 1; i < n; i++) {
+            left[i] = left[i - 1] * nums[i - 1];
+        }
+
+        // Update right array
+        right[n - 1] = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = right[i + 1] * nums[i + 1];
+        }
+
+        // Build product array
+        for (int i = 0; i < n; i++) {
+            prod[i] = left[i] * right[i];
+        }
+
+        return prod;
+    }
+}
