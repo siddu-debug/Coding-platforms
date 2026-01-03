af50cbfdef80e042b03ff5a801a8b2a3720558fa
Rajgiri Siddeshwar <siddurajgiri@gmail.com>
2026-01-03 15:44:11 +0530

    Time: 591 ms (55.86%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -0,0 +1,12 @@
+SELECT 
+    ROUND(
+        SUM(order_date = customer_pref_delivery_date) * 100.0 
+        / COUNT(DISTINCT customer_id),
+        2
+    ) AS immediate_percentage
+FROM Delivery
+WHERE (customer_id, order_date) IN (
+    SELECT customer_id, MIN(order_date)
+    FROM Delivery
+    GROUP BY customer_id
+);
