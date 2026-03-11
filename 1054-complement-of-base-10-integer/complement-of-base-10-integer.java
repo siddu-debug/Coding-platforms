3ae29cf0d0026c3ee9206e74a69d20c63d8cf9c5
Rajgiri Siddeshwar <siddurajgiri@gmail.com>
2026-03-11 17:31:27 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (50.09%) - LeetSync

---
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int bitwiseComplement(int n) {
+        
+        if (n == 0) {
+            return 1;
+        }
+
+        int mask = 0;
+
+        while (mask < n) {
+            mask = (mask << 1) | 1;
+        }
+
+        return mask ^ n;
+    }
+}
