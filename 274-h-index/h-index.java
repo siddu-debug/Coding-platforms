7ce2952c2b1d2134da4d19153741fc5f7ef85a0d
Rajgiri Siddeshwar <siddurajgiri@gmail.com>
2026-08-13 11:51:19 +0530

    Time: 5 ms (73.47%) | Memory: 43.8 MB (5.68%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,9 +1,11 @@
 class Solution {
     public int hIndex(int[] citations) {
+
+        int n = citations.length;
         Arrays.sort(citations);
-        int n =citations.length;
-        for(int i=0;i<n;i++){
-            int h=n-i;
+
+        for(int i = 0; i < n; i++ ){
+            int h = n - i;
             if(citations[i] >= h){
                 return h;
             }
